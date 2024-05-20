de7691b708f0c2e45e56abf3dfccda422dfda63c
Jeffrey Verdu <jeffverdu@gmail.com>
2024-05-20 14:10:04 -0300

    Actualización de estilos responsive

--- src/components/layout/header/HeroHome.jsx
+++ src/components/layout/header/HeroHome.jsx
@@ -3,9 +3,11 @@ import * as React from "react";
 export default function HeroHome() {
   return (
     <div className="hero-container">
-      <div className="hero-content"></div>
-      <div className="hero-heading ms-20">
-        <h2 className="text-white">El sitio para los amantes del cine</h2>
+      <div className="hero-content 2xl:h-[70rem]"></div>
+      <div className="hero-heading ms-[10%] 2xl:ms-[30%] 2xl:w-[60rem]">
+        <h2 className="text-white text-[3rem] md:text-[4rem] 2xl:text-[5rem]">
+          El sitio para los amantes del cine
+        </h2>
       </div>
     </div>
   );
